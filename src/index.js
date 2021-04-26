b14a9846146830b53f197423391f29f964b79cf8
Nguyen Dinh Phu <nguyendinhphu98@gmail.com>
2021-04-26 14:26:58 +0700

    ConsumAPI - tutorial

--- src/index.js
+++ src/index.js
@@ -1,41 +1,9 @@
 import configureStore from "./store/configureStore";
-import {
-  bugAdded,
-  bugResolved,
-  bugAssignedToUser,
-  getUnresolvedBugs,
-  getBugsByUser,
-} from "./store/bugs";
-import { projectAdded } from "./store/projects";
-import { userAdded } from "./store/users";
+import * as actions from "./store/api";
+import { loadBugs, addBug } from "./store/bugs";
 
 const store = configureStore();
 
-/*
-store.subscribe(() => console.log("Store changed"));
+store.dispatch(addBug({ description: "a" }));
 
-store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
-store.dispatch(projectAdded({ name: "Project 1" }));
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-store.dispatch(bugResolved({ id: 1 }));
-
-const bugs = getBugsByUser(2)(store.getState());
-console.log(bugs);
-*/
-
-// store.dispatch((dispatch, getState) => {
-//   // Call an API
-//   // When the promise is resolved => dispatch() the result of promise
-//   dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
-//   console.log(getState());
-//   // If the promise is rejected => dispatch()
-// });
-
-store.dispatch({
-  type: "error",
-  payload: { message: "An error occured" },
-});
+// setTimeout(() => store.dispatch(loadBugs()), 2000);
